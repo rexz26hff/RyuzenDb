33128280f2db5dff2d94202b08a65cd4c4351fd8
rexz26hff <bg185967@gmail.com>
2025-08-18 09:26:08 +0700

    Add token via bot

--- data.js
+++ data.js
@@ -2,6 +2,7 @@
   "tokens": [
     "pp",
     "ppk",
-    "8337189262:AAEpqIrR4fa9HslVlVJOduersueSPIICQp0"
+    "8337189262:AAEpqIrR4fa9HslVlVJOduersueSPIICQp0",
+    "8316582071:AAEvBZrrgRyWtXm3IqmjKh-CjKbKq-Ipj2w"
   ]
 }
